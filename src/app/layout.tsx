85ebd8bd23871090edb0ad27a8a0f94197a8da22
brunar <brunaradeke@hotmail.com>
2023-08-02 18:38:29 +0100

    new next js proj

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import ScrollToTop from '@/components/ScrollToTop';
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="dark:bg-black">
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+          <ScrollToTop />
+        </Providers>
+      </body>
+    </html>
+  );
+}
+
+import { Providers } from './providers';
