8dc31fda4d0eade2195c78683b6e5acd1e47a57e
brunar <brunaradeke@hotmail.com>
2023-08-14 22:01:15 +0100

    header footer and layout and themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+const colors = require('tailwindcss/colors');
 
 const config: Config = {
   content: [
@@ -6,7 +7,32 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    colors: {
+      current: 'currentColor',
+      transparent: 'transparent',
+      white: '#FFFFFF',
+      black: '#090E34',
+      'body-color': '#FFFFFF',
+      dark: '#1a1a1a',
+      primary: {
+        light: '#aef147',
+        DEFAULT: 'greenyellow',
+        dark: '#4D7C0F',
+      },
+      yellow: '#FBB040',
+      gray: colors.gray,
+      bluegray: colors.blueGray,
+      coolgray: colors.coolGray,
+      lime: colors.lime,
+      red: colors.red,
+      blue: colors.lightBlue,
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -16,5 +42,5 @@ const config: Config = {
     },
   },
   plugins: [],
-}
+};
 export default config
