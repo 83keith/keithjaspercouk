b60e5c8095775fa6f87ad8487579accac490f72a
83keith <keith_83@icloud.com>
2023-08-17 18:54:08 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,13 +4,13 @@ export NVM_DIR="$HOME/.nvm"
 
 
 rm -rf /home/ubuntu/autostage/*
-rm -rf /home/ubuntu/autostage/.*
 rm -rf /home/ubuntu/autostage
+
 mkdir -p /home/ubuntu/autostage/deployment-archive/
 echo $DEPLOYMENT_GROUP_ID
 echo $DEPLOYMENT_ID
 cp -R /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/* /home/ubuntu/autostage/
-cp -R /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/.* /home/ubuntu/autostage/
+cp -R /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive /home/ubuntu/autostage/
 
 cd /home/ubuntu/autostage/deployment-archive
 
@@ -19,8 +19,8 @@ cd /home/ubuntu/autostage/deployment-archive
 rm -rf /var/www/html/*
 rm -rf /var/www/html/.*
 
-cp /home/ubuntu/autostage/deployment-archive/.* . -R
-cp /home/ubuntu/autostage/deployment-archive/* . -R
+cp /home/ubuntu/autostage/deployment-archive/.* /var/www/html/ -R
+cp /home/ubuntu/autostage/deployment-archive/* /var/www/html/ -R
 chmod 777 /var/www/html/includes/smarty/
 
 #yarn install
