7b49f888ffd0289c28279aa9d32bba507a2fe6a0
83keith <keith_83@icloud.com>
2023-09-03 15:42:30 +0100

    Update Configuration.class.php

--- includes/Configuration.class.php
+++ includes/Configuration.class.php
@@ -1,5 +1,11 @@
 <?php
 DEFINE('DATABASE_HOST', 'localhost');
-DEFINE('DATABASE_USER', 'root');
-DEFINE('DATABASE_PASS', '');
+if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == 'localhost') {
+    DEFINE('DATABASE_USER', 'root');
+    DEFINE('DATABASE_PASS', '');
+} else {
+    DEFINE('DATABASE_USER', 'keithjaspercouk');
+    // YES this is a silly password (host rules in effect.)
+    DEFINE('DATABASE_PASS', 'keith');
+}
 DEFINE('DATABASE_NAME', 'keithjaspercouk');
