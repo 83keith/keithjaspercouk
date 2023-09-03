4d4ac498b944570cd6cb39304c82bda6d7a1dccf
83keith <keith_83@icloud.com>
2023-09-03 16:21:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ if (!isset($_GET['API'])) {
         curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
         try {
             $data = curl_exec($curlSession);
-            $data = str_replace('\n', '', $data);
+            $data = str_replace('\\', '', $data);
         } catch (Exception $e) {
             var_dump($e);
         }
